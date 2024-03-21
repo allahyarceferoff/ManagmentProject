ded60d4203a2688d2152e5e34ca550c9dc2ae713
allahyarceferoff <callahyar@inbox.ru>
2024-03-21 23:41:13 +0400

    add BaseLibrary Client Clientlibrary ServerLibrary dependies

---
+++ BaseLibrary/Entities/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace BaseLibrary.Entities
+{
+    public class ApplicationUser 
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+
+
+    }
+}
---
+++ ServerLibrary/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BaseLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ServerLibrary.Data
+{
+
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Branch> Branchs { get; set; }
+
+        public DbSet<Town> Towns { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+
+
+    }
+}
